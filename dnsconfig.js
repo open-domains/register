c524582c4dabc2b135adc6e078a7c7756f19b504
andrewstech <info@andrewstech.me>
2022-12-03 21:48:47 +0000

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -69,8 +69,9 @@ for (var idx in domains) {
   }
   
   if (domainData.record.MX) {
-    commit[domainData.domain].push(
-      CNAME(domainData.subdomain, "10", domainData.record.MX, proxyState) // https://stackexchange.github.io/dnscontrol/js#CNAME
+    for (var mx in domainData.record.MX) {
+      commit[domainData.domain].push(
+        MX(domainData.subdomain, "10", domainData.record.MX[mx], proxyState) // https://stackexchange.github.io/dnscontrol/js#CNAME
     )
   }
 
