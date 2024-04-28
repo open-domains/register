552eefc1347590650eec35081f5d6c3818d5c4b2
danlp6tr <168312464+danlp6tr@users.noreply.github.com>
2024-04-28 06:39:54 +0100

    Create repo.is-local.org.js

---
+++ domains/repo.is-local.org.js
@@ -0,0 +1,17 @@
+{
+    "description": "Linux Distro and apt host.",
+
+    "domain": "is-local.org",
+    "subdomain": "repo",
+
+    "owner": {
+        "repo": "https://github.com/username/repo",
+        "email": "danlp6.com@outlook.de"
+    },
+
+    "record": {
+        "A": ["212.132.139.47"]
+    },
+
+    "proxied": false
+}
