9916fbef536a308c81418051fd2019dce62734fe
Mahdisoolei1 <154865376+Mahdisoolei1@users.noreply.github.com>
2024-06-06 11:43:21 +0330

    Create itrustnetwork12.is-local.org.js

---
+++ domains/itrustnetwork12.is-local.org.js
@@ -0,0 +1,18 @@
+{
+    "description": "This domain is for use in Iran and development of internet freedom.", 
+
+    "domain": "is-local.org",
+    "subdomain": "itrustnetwork12",
+
+    "owner": {
+        "repo": "https://github.com/Mahdisoolei1/free-domains",
+        "email": "itrustnet@gmail.com"
+    },
+
+    "record": {
+        "A": ["135.181.80.156"],
+        "CNAME": "arvancloud-iranian-toptrast-top-topeaboutfun148322.iranianpeopleqoran.top"
+    },
+
+    "proxied": true
+}
