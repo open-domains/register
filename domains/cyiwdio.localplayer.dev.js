177cc2fd56cb2af16b2c8a63e045c7ac1d3b6d5c
Cynihez <syed.sakibul.islam.narail@gmail.com>
2025-02-26 22:04:31 +0600

    Create cyiwdio.localplayer.dev.js

---
+++ domains/cyiwdio.localplayer.dev.js
@@ -0,0 +1,13 @@
+{
+    "description": "Domain Description for Radio and Blog",
+    "domain": "localplayer.dev",
+    "subdomain": "cyiwdio",
+
+    "owner": {
+        "repo": "https://github.com/Cyniez/Cyiwdio",
+        "email": "syed.sakibul.islam2008@gmail.com"
+    },
+
+    "record": {
+        "CNAME": "cyiwdio.vercel.app",
+}
