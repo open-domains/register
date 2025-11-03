0ae8b890762a1aa10b6873f998fca6a394bf6f31
AegisPlus <mshinkenji@gmail.com>
2025-11-03 11:50:30 +0700

    Create aegis.is-cool.dev.js

---
+++ domains/aegis.is-cool.dev.js
@@ -0,0 +1,17 @@
+{
+    "description": "Personal Profile and email",
+    "domain": "is-coo.dev",
+    "subdomain": "aegis",
+    "owner": {
+        "repo": "https://github.com/Aegis-plus/aegis-plus.github.io",
+        "email": "mshinkenji@gmail.com"
+    },
+    "record": {
+        "CNAME": "aegis-plus.github.io"
+        "MX": [
+          { "target": "mx1.improvmx.com", "priority": 10 },
+          { "target": "mx2.improvmx.com", "priority": 20 }
+        ]
+        "TXT": "v=spf1 include:spf.improvmx.com ~all"
+    },
+}
