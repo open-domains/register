92b5d29b0376a5a746635c08909f6e5081465a09
zxgvdh9jqb-wq <zxgvdh9jqb@privaterelay.appleid.com>
2025-11-17 23:07:31 +0000

    apply
    The Manchester Community Creative Reuse Initiative (MCCRI) is a registered cultural and environmental organisation dedicated to promoting sustainability and creative reuse of materials. Founded in 2014 in Manchester's vibrant Northern Quarter, MCCRI has become a cornerstone of the local environmental movement.

---
+++ communitycreativereuseinitiative.is-a-fullstack.dev.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: "my website for a charity event",  
+  domain: "is-a-fullstack.dev",
+  subdomain: "communitycreativereuseinitiative"
+  owner: {
+    repo: "https://github.com/zxgvdh9jqb-wq/rebottle-website",
+    email: "mxxdlt777@outlook.com"
+  },
+  record: {
+    CNAME: "zxgvdh9jqb-wq.github.io"  
+  }
+})
